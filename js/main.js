db7bdbddbce0d7d2031c2141c7206a363cb7235f
huamianao <1762871539@qq.com>
2017-08-30 09:05:34 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,364 @@
+
+var name ='王花花';
+alert('你好' + name);
+var a = 1; //把1赋值给a
+var b = 2;
+//var c = 3;
+"王花花"
+'王花花'
+'王花花说："你好"'
+'he\'s'
+'啊' + 
+'祖国';
+[
+'左胸疼是怎么回事_有问必答_快速问医生',
+'胸疼是怎么回事?_育儿问答_宝宝树',
+'心慌失眠胸疼大部分人不知道的调理方法'
+]
+var o = {
+	a: 1,
+	b: 2
+};
+o.a;
+var x = o.a + o.b;
+1;
+1.2;
+-1;
+'王花花';
+"王花花"
+"王花花说：'你好'";
+'it\'s';
+"it's";
+'啊' +
+'祖国';
+[
+1,2,3
+]
+var q ={
+	a: 2,
+	b: 3
+};
+q.a;
+q.b;
+var w =q.a + q.b;
+var m ='yo.';
+m = 1;
+m = true;
+m = {};
+m =[];
+console.log(m);
+console.log(typeof m);
+var t = 23;
+var r =true;
+var f =[];
+var h ={};
+var g = 1.4;
+var j = false;
+var t = '123';
+var d = 'we';
+var t = '124';
+var u = 34;
+var fg = '123';
+var tg = 23;
+var we = 'er';
+
+var Isidmin = false;
+    Isidmin = 0;
+    Isidmin = '';
+    Isidmin = NaN;
+    Isidmin = null;
+    Isidmin = undefined;
+if (Isidmin) {
+	console.log ("他是管理员");
+}else {
+	console.log("他不是管理员")
+}
+var Isidmin = true;
+if (Isidmin){
+	console.log('他是管理员'); 
+}else{
+	console.log('他不是管理员')
+};
+var Isgoodman = false;
+if(Isgoodman){
+	console.log('他是好人');
+}else{
+	console.log('他是坏人');
+};
+
+var isgoodman = false;
+if(isgoodman){
+	console.log("他是好人");
+}else{
+	console.log('他不是好人')
+};
+var a = 1;
+if(a==1){
+	console.log('你是好学生')
+}else{
+	console,log('你不是好学生')
+};
+
+var isgoodman = true;
+if(isgoodman){
+	console.log('他是好人')
+}else{
+	console.log('他不是好人')
+};
+if('今天是星期一'){
+	//..
+}else{
+	if('今天是星期二'){
+		//..
+	}else{
+		if('今天是星期三'){
+			//..
+		}else{
+			//..
+		}
+	}
+	
+}
+
+
+if('今天是星期一'){
+	//..
+}else if('如果今天是星期二'){
+	//..
+}else if('如果今天是星期三'){
+	//..
+}else{
+	//...
+};
+
+
+var day =6;
+switch(day){
+	case 1:
+	console.log('今天吃牛肉面');
+	break;
+	case 2:
+	console.log('今天吃挂面');
+	break;
+	case 3:
+	console.log('今天吃土豆拌饭');
+	break;
+	default:
+	console.log('今天吃茄汁面');
+	break;
+}
+var month;
+switch(month){
+	case 1:
+	console.log('上学');
+	break;
+	case 2:
+	console.log('上学');
+	break;
+	case 3:
+	console.log('上学');
+	break;
+	case 4:
+	console.log('上学');
+	break;
+	case 5:
+	console.log('休息');
+	break;
+	default:
+	console.log('购物');
+	break;
+}
+
+var year = 4;
+switch(year){
+	case 1:
+	console.log('挣钱');
+	break;
+	case 2:
+	console.log('挣钱');
+	break;
+	case 3:
+	console.log('买房');
+	break;
+	case 4:
+	console.log('娶媳妇');
+	break;
+};
+1 + 1
+'1'+'1'
+12 % 3 //0
+11 % 3 //2
+10 % 3 //1
+var loginCount = 0;
+//loginCount = loginCount + 1;
+loginCount++;
+loginCount++;
+console.log(loginCount);
+loginCount = 0;
+loginCount--;
+console.log('loginCount:' , loginCount);
+console.log("1 > 2:" + (1 > 2));
+console.log('5 > 4:' + (5 > 4));
+console.log('1 >= 1:' + (1 >=1));
+console.log('1 <= 1:' + (1 <=1));
+console.log('1 == 2:' + (1==2));
+console.log('1 == 1:' + (1==1));
+console.log("'1' == 1:" + ('1'==1));
+console.log("'1' ===1:" + ('1'===1));
+console.log('1 != 2:' + (1 != 2));
+console.log('1 != 1:' + (1 != 1));
+loginCount = 3;
+//loginCount = loginCount + 2;
+loginCount += 2;
+console.log('loginCount:' + loginCount);
+var   他是制单人 = true;
+      他是审核员 = false;
+      
+  if(他是制单人 && 他是审核员){
+  	console.log('转账成功')
+  }else{
+  	console.log('转账失败，权限不足')
+ };
+  if(他是制单人 || 他是审核员){
+  	console.log('转账成功')
+  }else{
+  	console.log('转账失败，权限不足')
+ };
+  loginCount = 3;
+  loginCount++;
+  console.log('loginCount:',loginCount);
+  var i;
+ for (i = 1;i <= 100;i++ ){
+  	console.log(i);
+ };
+
+var i;
+for (i=1;i<100;i=i+2){
+	console.log(i);
+};
+var i;
+for (i=1;i<=45;i++){
+	console.log(i);
+};
+ var i;
+ for (i=2;i>=100;i++){
+ 	console.log(i);
+ };
+ var i;
+ var result =['百度一下,你就知道',
+              '百度地图',
+              '百度地图',
+              '百度地图',
+              '百度新闻搜索--全球最大的中文新闻平台'
+ ];
+for (i=0; i<result.length; i++){
+console.log(result[i]);
+};
+var i;
+var result =[
+'淘宝网-淘宝贝,淘不停!淘,你喜欢! ',
+'淘宝网- 淘!我喜欢',
+'宝网商城,「天猫超市」钜"惠"来袭,精选好货!'
+];
+for (i=0; i<3; i++){
+	console.log(result[i]);
+};
+var attempt =0;
+while(attempt<5){
+	attempt++;
+	console.log(attempt);
+};
+console.log('loop stopped');
+
+var attempt =0;
+while(attempt<5){
+	console.log(attempt);
+	attempt++;
+}
+console.log('loop stopped');
+
+var a = 0;
+while(a<7){
+	a++;
+	console.log(a);
+};
+
+function 做饭 (){
+	if(锅没洗){
+		//叫外卖
+	}else if(心情不好){
+		//叫外卖
+	}else{
+		//自己做饭
+	}
+};
+
+function buy (钱){
+	if(!钱){
+		alert('警察叔叔就是这个人');
+	}else{
+		alert('钱拿好，慢走!');
+	}
+};
+buy(10);
+
+var a =1;
+console.log(a);
+function fun(){
+	var b = 2;
+	console.log(b);
+	console.log(a);
+
+};
+fun();
+
+;(function (){})();
+
+alert(1);
+var  r = confirm('你是否已年满18周岁');
+console.log('r:' + r);
+var name = prompt('你的名字是什么?');
+console.log('name:' + name);
+alert("你将获得最漂亮的❀");
+var t = confirm('你是否已经满20周岁？');
+console.log('t:' + t);
+var h = confirm("你是否已经到结婚年龄?");
+console.log('h:' + h);
+var sex = prompt('你的性别是？');
+console.log('sex:' + sex);
+var high =prompt('你的身高是多少？');
+console.log('high:' + high);
+var height = prompt('你的体重是多少？');
+console.log('height:' + height);
+//setTimeout(function(){console.log('2s时间到了')},2000);
+setTimeout(function(){console.log('时间到了')},1000);
+count = 0;
+ timer = setInterval(function(){
+ 	count++;
+ 	if(count > 3){
+ 		clearInterval(timer);
+ 		return;
+ 	};
+	console.log('yo' + count);
+},1000);
+
+function add (a,b){
+	return a + b;
+};
+var q = add(3,5);
+console.log('q:' + q);
+function 减 (a,b,c){
+	return a-b-c;
+};
+Z = 减(6,7,4);
+console.log('Z:' + Z);
+
+var user = {
+	name: 'liza',
+	sex: 'woman',
+	age:20
+};
+console.log('user:' , user);
+user.age = 21;
+console.log('user:' , user);
+
